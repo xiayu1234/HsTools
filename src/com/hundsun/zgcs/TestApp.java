59cc4ca04bb4651ff2e1aee047761bd22e69a870
xiayu20540 <xiayu20540@xiayu20540.hs.handsome.com.cn>
2018-01-15 16:41:55 +0800

    完成统计方法的编写并测试通过

--- src/com/hundsun/zgcs/TestApp.java
+++ src/com/hundsun/zgcs/TestApp.java
@@ -20,13 +20,27 @@ public class TestApp {
 		File dic = new File(loc2);
 
 		ArrayList<String> list = ExcelUtil.getColumByName(file, "测试执行人");
+		/**
+		for (int i = 0; i < list.size(); i++) {
+			if(list.get(i).indexOf("夏雨") != -1) {
+				System.out.println(list.get(i));
+			}
+			
+		}
+		System.out.println(list.size());
+		*/
 		ArrayList<ArrayList<String>> dicList = ExcelUtil.getColumnList(dic);
+		//System.out.println(dicList.size());
 		HashMap<String, Integer> map = TjUtil.getCount(list, dicList);
+	//	System.out.println(map.size());
+		
+		
 		for (int i = 0; i < dicList.size(); i++) {
 			for (int j = 0; j < dicList.get(i).size(); j++) {
 				System.out.println(dicList.get(i).get(j) + ":" + map.get(dicList.get(i).get(j)));
 			}
 		}
+		
 	}
 
 }
--- src/com/hundsun/zgcs/TjUtil.java
+++ src/com/hundsun/zgcs/TjUtil.java
@@ -3,6 +3,8 @@ package com.hundsun.zgcs;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.sun.media.sound.SoftMixingSourceDataLine;
+
 /**
  * 工具类
  * 
@@ -30,7 +32,6 @@ public class TjUtil {
 		Boolean flag = false;
 		for (int i = 0; i < column.size(); i++) {
 			for (int j = 0; j < list.size(); j++) {
-
 				if (flag) {
 					// 小组数量统计
 					if (map.containsKey(list.get(j).get(0))) {
@@ -43,13 +44,14 @@ public class TjUtil {
 				
 				// 人员数据统计
 				for (int p = 0; p < list.get(j).size(); p++) {
+					
 					// 判断人员是否在测试执行人里
-					if (column.indexOf(list.get(j).get(p)) != -1) {
+					if (column.get(i).indexOf(list.get(j).get(p)) != -1) {
 						// 判断键值是否存在
 						if (map.containsKey(list.get(j).get(p))) {
 							map.put(list.get(j).get(p), map.get(list.get(j).get(p)) + 1);
 						} else {
-							map.put(list.get(j).get(p), 1);
+							map.put(list.get(j).get(p), 1);	
 						}
 						flag = true;
 					}
